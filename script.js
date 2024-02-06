1c2e7e87df841002683a604072e7c9dd62df2abd
Ricardo Vargas V <vargasvargas.ric@gmail.com>
2024-02-06 06:18:10 -0500

    js

---
+++ script.js
@@ -0,0 +1,46 @@
+// Html Settings
+const board = document.getElementById("board");
+const scoreBoard = document.getElementById("scoreBoard");
+const startButton = document.getElementById("start");
+const gameOver = document.getElementById("gameOver");
+
+// Game Settings
+const boardSize = 10;
+const gameSpeed = 100;
+const squareTypes = {
+  emptySquare: 0,
+  snakeSquare: 1,
+  foodSquare: 2,
+};
+
+const directions = {
+  ArrowUp: -10,
+  ArrowDown: 10,
+  ArrowRight: 1,
+  ArrowLeft: -1,
+};
+
+// Game Variables
+let snake;
+let score;
+let direction;
+let boardSquares;
+let emptySquares;
+let moveInterval;
+
+const setGame = () => {
+  snake = ["00", "01", "02", "03"];
+  score = snake.length;
+  direction = "ArrowRight";
+  boardSquares = Array.from(Array(boardSize), () =>
+    new Array(boardSize).fill(squareTypes.emptySquare)
+  );
+
+  console.log(boardSquares);
+};
+
+const startGame = () => {
+  setGame();
+};
+
+startButton.addEventListener("click", startGame);
